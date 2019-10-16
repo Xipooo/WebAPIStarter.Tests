24dfe4ad7468e9b1757bce48cf50211df0fe61a5
Steve Bishop <xipooo@gmail.com>
2019-10-16 11:15:18 -0700

    Modified test for DI

--- CustomerControllerTests.cs
+++ CustomerControllerTests.cs
@@ -12,11 +12,14 @@ namespace WebAPIStarter.Tests
 {
     public class CustomerControllerTests
     {
+
+        private Mock<ICustomerService> mockService = new Mock<ICustomerService>();
+
         [Fact]
         public void GetAll_WhenCalled_ReturnsOKResult()
         {
             // Arrange
-            CustomerController customerController = new CustomerController();
+            CustomerController customerController = new CustomerController(mockService.Object);
 
             // Act
             var getResult = customerController.GetAll();
@@ -29,14 +32,14 @@ namespace WebAPIStarter.Tests
         public void Create_WhenCalled_WithValidCustomer_ReturnsCreatedAtActionResult()
         {
             // Given
-            CustomerController customerController = new CustomerController();
+            CustomerController customerController = new CustomerController(mockService.Object);
             Customer newCustomer = new Customer
             {
                 FirstName = "Bilbo",
                 LastName = "Baggins",
                 Email = "Bilbo@TheShire.net"
             };
-
+            mockService.Setup(serv => serv.Add(newCustomer)).Returns(newCustomer);
             // When
             var getResult = customerController.Create(newCustomer);
 
@@ -46,15 +49,7 @@ namespace WebAPIStarter.Tests
 
         [Fact]
         public void GetOne_WhenCalled_ReturnsCustomer()
-        {
-            // Arrange
-            // var customers = new List<Customer> {
-            //    new Customer { Id = 1, FirstName = "Steve", LastName = "Bishop", Email = "steve.bishop@galvanize.com" },
-            //    new Customer { Id = 2, FirstName = "Marla", LastName = "Gonzales", Email = "mGone@home.net" },
-            //    new Customer { Id = 3, FirstName = "Alfred", LastName = "Pennyworth", Email = "alfred@thebatcave.org" }
-            // };
-        
-            var mockService = new Mock<ICustomerService>();
+        {        
             var fakeCustomer = new Customer { Id = 1, FirstName = "Steve", LastName = "Bishop", Email = "steve.bishop@galvanize.com" };
             mockService.Setup(serv => serv.GetOne(1)).Returns(fakeCustomer);
             CustomerController SUT = new CustomerController(mockService.Object);
